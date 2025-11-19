32a83de7ca24a5dd98a91cdba235e7ed5feb1193
vzeufack <43120988+vzeufack@users.noreply.github.com>
2025-11-19 08:22:08 -0500

    Add data processing script for customer landing to trusted

---
+++ customer_landing_to_trusted.py
@@ -0,0 +1,36 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
+import re
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
+
+# Script generated for node Customer Landing
+CustomerLanding_node1763549559839 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-lake-house/customer/landing/"], "recurse": True}, transformation_ctx="CustomerLanding_node1763549559839")
+
+# Script generated for node Privacy Filter
+PrivacyFilter_node1763549779295 = Filter.apply(frame=CustomerLanding_node1763549559839, f=lambda row: (not(row["sharewithresearchasofdate"] == 0)), transformation_ctx="PrivacyFilter_node1763549779295")
+
+# Script generated for node Customer Trusted
+EvaluateDataQuality().process_rows(frame=PrivacyFilter_node1763549779295, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1763548595888", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+CustomerTrusted_node1763549091186 = glueContext.getSink(path="s3://stedi-lake-house/customer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="CustomerTrusted_node1763549091186")
+CustomerTrusted_node1763549091186.setCatalogInfo(catalogDatabase="stedi",catalogTableName="customer_trusted")
+CustomerTrusted_node1763549091186.setFormat("json")
+CustomerTrusted_node1763549091186.writeFrame(PrivacyFilter_node1763549779295)
+job.commit()
