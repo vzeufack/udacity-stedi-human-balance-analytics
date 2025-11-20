93494639b3d29cce14ebe425bc9d667201c0c0d8
vzeufack <43120988+vzeufack@users.noreply.github.com>
2025-11-20 07:49:17 -0500

    Refactor data processing with SQL query and DynamicFrame

--- customer_landing_to_trusted.py
+++ customer_landing_to_trusted.py
@@ -5,8 +5,13 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsgluedq.transforms import EvaluateDataQuality
-import re
+from awsglue import DynamicFrame
 
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -22,15 +27,19 @@ DEFAULT_DATA_QUALITY_RULESET = """
 """
 
 # Script generated for node Customer Landing
-CustomerLanding_node1763549559839 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-lake-house/customer/landing/"], "recurse": True}, transformation_ctx="CustomerLanding_node1763549559839")
+CustomerLanding_node1763642550752 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_landing", transformation_ctx="CustomerLanding_node1763642550752")
 
 # Script generated for node Privacy Filter
-PrivacyFilter_node1763549779295 = Filter.apply(frame=CustomerLanding_node1763549559839, f=lambda row: (not(row["sharewithresearchasofdate"] == 0)), transformation_ctx="PrivacyFilter_node1763549779295")
+SqlQuery0 = '''
+select * from myDataSource
+where shareWithResearchAsOfDate IS NOT NULL;
+'''
+PrivacyFilter_node1763642586549 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":CustomerLanding_node1763642550752}, transformation_ctx = "PrivacyFilter_node1763642586549")
 
 # Script generated for node Customer Trusted
-EvaluateDataQuality().process_rows(frame=PrivacyFilter_node1763549779295, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1763548595888", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+EvaluateDataQuality().process_rows(frame=PrivacyFilter_node1763642586549, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1763548595888", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
 CustomerTrusted_node1763549091186 = glueContext.getSink(path="s3://stedi-lake-house/customer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="CustomerTrusted_node1763549091186")
 CustomerTrusted_node1763549091186.setCatalogInfo(catalogDatabase="stedi",catalogTableName="customer_trusted")
 CustomerTrusted_node1763549091186.setFormat("json")
-CustomerTrusted_node1763549091186.writeFrame(PrivacyFilter_node1763549779295)
+CustomerTrusted_node1763549091186.writeFrame(PrivacyFilter_node1763642586549)
 job.commit()
