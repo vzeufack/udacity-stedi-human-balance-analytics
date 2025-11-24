a8b0181eb99b8b2072501fd39504623797c153c2
vzeufack <43120988+vzeufack@users.noreply.github.com>
2025-11-24 08:19:23 -0500

    Add ETL script for customer data processing

---
+++ customer_trusted_to_curated.py
@@ -0,0 +1,60 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
+from awsglue import DynamicFrame
+
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
+
+# Script generated for node Customer Trusted
+CustomerTrusted_node1763643612782 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_trusted", transformation_ctx="CustomerTrusted_node1763643612782")
+
+# Script generated for node Accelerometer Landing
+AccelerometerLanding_node1763643575336 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_landing", transformation_ctx="AccelerometerLanding_node1763643575336")
+
+# Script generated for node Join
+Join_node1763643636093 = Join.apply(frame1=CustomerTrusted_node1763643612782, frame2=AccelerometerLanding_node1763643575336, keys1=["email"], keys2=["user"], transformation_ctx="Join_node1763643636093")
+
+# Script generated for node Privacy filter
+SqlQuery0 = '''
+select * from myDataSource
+where timestamp >= shareWithResearchAsOfDate;
+'''
+Privacyfilter_node1763643992315 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":Join_node1763643636093}, transformation_ctx = "Privacyfilter_node1763643992315")
+
+# Script generated for node Drop fields and remove duplicates
+SqlQuery1 = '''
+select distinct customername, phone, birthday,
+serialnumber, registrationdate, lastupdatedate,
+sharewithresearchasofdate, sharewithpublicasofdate,
+sharewithfriendsasofdate from myDataSource
+'''
+Dropfieldsandremoveduplicates_node1763989137741 = sparkSqlQuery(glueContext, query = SqlQuery1, mapping = {"myDataSource":Privacyfilter_node1763643992315}, transformation_ctx = "Dropfieldsandremoveduplicates_node1763989137741")
+
+# Script generated for node Amazon S3
+EvaluateDataQuality().process_rows(frame=Dropfieldsandremoveduplicates_node1763989137741, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1763643375558", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AmazonS3_node1763644235527 = glueContext.getSink(path="s3://stedi-lake-house/customer/curated/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1763644235527")
+AmazonS3_node1763644235527.setCatalogInfo(catalogDatabase="stedi",catalogTableName="customer_curated")
+AmazonS3_node1763644235527.setFormat("json")
+AmazonS3_node1763644235527.writeFrame(Dropfieldsandremoveduplicates_node1763989137741)
+job.commit()
